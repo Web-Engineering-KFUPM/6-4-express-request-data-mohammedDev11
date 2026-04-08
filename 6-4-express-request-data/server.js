a1fe96abd549b229852cb961b234f11787bc9fc3
John Doe <johndoe@email.com>
2026-04-08 08:36:31 +0300

    Complete TODO-04

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -138,6 +138,20 @@ app.get("/profile/:first/:last", (req, res) => {
   });
 });
 // Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+  const num = Number(userId);
+
+  if (!Number.isFinite(num) || num <= 0) {
+    return res.status(400).json({
+      ok: false,
+      error: "userId must be positive number",
+    });
+  }
+
+  req.userIdNum = num;
+
+  next();
+});
 
 // Route params: /users/:userId route
 
